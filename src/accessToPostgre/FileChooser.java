e97921d1d4c9e775162813922086c7a4d1f28520
Nizar <therealnizarbenalla@gmail.com>
2022-05-01 22:17:40 +0000

    created FileChooser class

---
+++ src/accessToPostgre/FileChooser.java
@@ -0,0 +1,18 @@
+package accessToPostgre;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+public class FileChooser {
+
+		  public static JFileChooser createFilePicker() {
+		    JFileChooser chooser = new JFileChooser();
+		    chooser.setCurrentDirectory(new File("."));
+		    chooser.setCurrentDirectory(new File("."));
+		    chooser.setDialogTitle("choosertitle");
+		    chooser.setAcceptAllFileFilterUsed(false);
+		    chooser.addChoosableFileFilter(new FileNameExtensionFilter("Microsoft Access", "accdb"));
+
+		    return chooser;
+		  }
+		}
